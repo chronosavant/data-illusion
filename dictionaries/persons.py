1eb1b27faf14529be4166825366e69d7176e2efe
chronosavant <anton@chronosavant.ru>
2026-03-19 20:29:53 +0300

    Добавлена генерация отчеств

--- dictionaries/persons.py
+++ dictionaries/persons.py
@@ -21,14 +21,21 @@ def persons():
         for name, sex in tsv_reader:
             names[sex].append(name)
 
+    patronymics = { 'M': [], 'F': []}
+    with open('data/patronymics.tsv', 'r', newline='', encoding='utf-8') as file_in:
+        tsv_reader = csv.reader(file_in, delimiter='\t')
+        for patronymic, sex in tsv_reader:
+            patronymics[sex].append(patronymic)
+
     output_persons = []
     for _ in range(1000):
         last_name = random.choice(last_names)
-        if last_name[1] == 'M':
-            sex = 'Муж'
-        else:
-            sex = 'Жен'
-        output_persons.append([last_name[0], random.choice(names[last_name[1]]), sex])
+        sex = 'Муж' if last_name[1] == 'M' else 'Жен'
+        output_persons.append([last_name[0],
+                               random.choice(names[last_name[1]]),
+                               random.choice(patronymics[last_name[1]]),
+                               sex
+                              ])
 
     with open('output/dict_persons.tsv', 'w', newline='', encoding='utf-8') as file_out:
         tsv_writer = csv.writer(file_out, delimiter='\t', lineterminator='\n')
